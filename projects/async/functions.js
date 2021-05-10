0801b38a640b6bec229c3f31c9ceffb8574a63ec
Sergey S <s.n.spassiy@gmail.com>
2021-05-10 15:47:10 +0300

    6

--- projects/async/functions.js
+++ projects/async/functions.js
@@ -2,27 +2,52 @@
 
 /*
  Задание 1:
-
  Функция должна возвращать Promise, который должен быть разрешен через указанное количество секунд
-
  Пример:
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
-function delayPromise(seconds) {}
+function delayPromise(seconds) {
+  const milliseconds = seconds * 1000;
+
+  return new Promise((resolve) => {
+    setTimeout(function () {
+      resolve();
+    }, milliseconds);
+  });
+}
 
 /*
- Задание 2:
+   Задание 2:
+   2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+   Массив городов можно получить отправив асинхронный запрос по адресу
+   https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+   2.2: Элементы полученного массива должны быть отсортированы по имени города
+   Пример:
+     loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
+   */
+function loadAndSortTowns() {
+  return new Promise((resolve) => {
+    async function getCities() {
+      const url =
+        'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json';
+      const response = await fetch(url);
 
- 2.1: Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+      const cities = await response.json();
 
- Массив городов можно получить отправив асинхронный запрос по адресу
- https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+      cities.sort(function (a, b) {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      });
+      return cities;
+    }
 
- 2.2: Элементы полученного массива должны быть отсортированы по имени города
-
- Пример:
-   loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
- */
-function loadAndSortTowns() {}
+    resolve(getCities());
+  });
+}
 
 export { delayPromise, loadAndSortTowns };
--- projects/async/index.js
+++ projects/async/index.js
@@ -2,18 +2,13 @@
  Страница должна предварительно загрузить список городов из
  https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
  и отсортировать в алфавитном порядке.
-
  При вводе в текстовое поле, под ним должен появляться список тех городов,
  в названии которых, хотя бы частично, есть введенное значение.
  Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
-
  Во время загрузки городов, на странице должна быть надпись "Загрузка..."
  После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
-
  Разметку смотрите в файле towns.html
-
  Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
-
  *** Часть со звездочкой ***
  Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
  то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
@@ -23,7 +18,6 @@
 /*
  homeworkContainer - это контейнер для всех ваших домашних заданий
  Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
-
  Пример:
    const newDiv = document.createElement('div');
    homeworkContainer.appendChild(newDiv);
@@ -34,25 +28,31 @@ import './towns.html';
 const homeworkContainer = document.querySelector('#app');
 
 /*
- Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
-
- Массив городов пожно получить отправив асинхронный запрос по адресу
- https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
- */
-function loadTowns() {}
+    Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+    Массив городов пожно получить отправив асинхронный запрос по адресу
+    https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+    */
+function loadTowns() {
+  return fetch('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+    .then((res) => res.json())
+    .then((json) => {
+      return json.sort((a, b) => (a.name > b.name ? 1 : -1));
+    });
+}
 
 /*
- Функция должна проверять встречается ли подстрока chunk в строке full
- Проверка должна происходить без учета регистра символов
-
- Пример:
-   isMatching('Moscow', 'moscow') // true
-   isMatching('Moscow', 'mosc') // true
-   isMatching('Moscow', 'cow') // true
-   isMatching('Moscow', 'SCO') // true
-   isMatching('Moscow', 'Moscov') // false
- */
-function isMatching(full, chunk) {}
+    Функция должна проверять встречается ли подстрока chunk в строке full
+    Проверка должна происходить без учета регистра символов
+    Пример:
+      isMatching('Moscow', 'moscow') // true
+      isMatching('Moscow', 'mosc') // true
+      isMatching('Moscow', 'cow') // true
+      isMatching('Moscow', 'SCO') // true
+      isMatching('Moscow', 'Moscov') // false
+    */
+function isMatching(full, chunk) {
+  return full.toLowerCase().indexOf(chunk.toLowerCase()) !== -1;
+}
 
 /* Блок с надписью "Загрузка" */
 const loadingBlock = homeworkContainer.querySelector('#loading-block');
@@ -67,8 +67,49 @@ const filterInput = homeworkContainer.querySelector('#filter-input');
 /* Блок с результатами поиска */
 const filterResult = homeworkContainer.querySelector('#filter-result');
 
-retryButton.addEventListener('click', () => {});
+retryButton.addEventListener('click', () => {
+  tryToLoad();
+});
 
-filterInput.addEventListener('input', function () {});
+filterInput.addEventListener('input', function () {
+  displayCities(this.value);
+});
+
+loadingFailedBlock.classList.add('hidden');
+filterBlock.classList.add('hidden');
+
+let cities = [];
+
+function tryToLoad() {
+  loadTowns()
+    .then((loadedCities) => {
+      cities = loadedCities;
+      loadingBlock.classList.add('hidden');
+      filterBlock.classList.remove('hidden');
+    })
+    .catch(() => {
+      loadingBlock.classList.add('hidden');
+      loadingFailedBlock.classList.remove('hidden');
+      filterBlock.classList.add('hidden');
+    });
+}
+
+function displayCities(inputText) {
+  filterResult.innerHTML = '';
+
+  if (inputText) {
+    const result = cities.filter((city) => {
+      return isMatching(city.name, inputText);
+    });
+
+    result.forEach((city) => {
+      const cityDiv = document.createElement('div');
+      cityDiv.innerText = city.name;
+      filterResult.appendChild(cityDiv);
+    });
+  }
+}
+
+tryToLoad();
 
 export { loadTowns, isMatching };
